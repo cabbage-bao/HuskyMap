d732f61f563a75b380d6632765106c904ab08bfa
24321 <cybao@uw.edu>
2019-11-05 16:18:56 -0800

    HW5_v2

--- kdtree/KDTreePointSet.java
+++ kdtree/KDTreePointSet.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class KDTreePointSet implements PointSet {
 
-    private KDNode kdRoot = null;
+    private KDNode kdRoot;
 
     private double bestX;
     private double bestY;
@@ -15,12 +15,15 @@ public class KDTreePointSet implements PointSet {
      *               after construction don't affect the point set)
      **/
     public KDTreePointSet(List<Point> points) {
+        int n = points.size();
+        if (n == 0 || points == null) {
+            throw new IllegalArgumentException();
+        }
         kdRoot = new KDNode(points.get(0));
-        for (int i = 1; i < points.size(); i++) {
+        for (int i = 0; i < n; i++) {
             kdRoot.add(kdRoot, points.get(i), 1);
         }
     }
-
     /**
      * Returns the point in this set closest to (x, y) in (usually) O(log N) time,
      * where N is the number of points in this set.
@@ -35,12 +38,6 @@ public class KDTreePointSet implements PointSet {
         if (root == null) {
             return;
         }
-        if (root.point.equals(new Point(x, y))) {
-            minDistance = 0;
-            bestX = x;
-            bestY = y;
-            return;
-        }
 
         double temp = root.point.distanceSquaredTo(x, y);
         if (temp < minDistance) {
@@ -52,14 +49,26 @@ public class KDTreePointSet implements PointSet {
         if (layer % 2 == 1) {
             if (x < root.point.x()) {
                 recursion(root.left, x, y, layer + 1);
+                if (Math.abs(root.point.x() - x) < minDistance) {
+                    recursion(root.right, x, y, layer + 1);
+                }
             } else {
                 recursion(root.right, x, y, layer + 1);
+                if (Math.abs(root.point.x() - x) < minDistance) {
+                    recursion(root.left, x, y, layer + 1);
+                }
             }
         } else if (layer % 2 == 0) {
             if (y < root.point.y()) {
                 recursion(root.left, x, y, layer + 1);
+                if (Math.abs(y - root.point.y()) < minDistance) {
+                    recursion(root.right, x, y, layer + 1);
+                }
             } else {
                 recursion(root.right, x, y, layer + 1);
+                if (Math.abs(y - root.point.y()) < minDistance) {
+                    recursion(root.left, x, y, layer + 1);
+                }
             }
         }
     }
@@ -68,7 +77,6 @@ public class KDTreePointSet implements PointSet {
         Point point;
         KDNode left;
         KDNode right;
-
         public KDNode(Point point) {
             this.point = point;
             this.left = null;
@@ -81,6 +89,9 @@ public class KDTreePointSet implements PointSet {
         public KDNode add(KDNode root, Point addPoint, int layer) {
             if (root == null) {
                 return new KDNode(addPoint);
+            }
+            if (root.point.equals(addPoint)) {
+                return root;
             }
             if (layer % 2 == 1) {
                 if (addPoint.x() < root.point.x()) {
